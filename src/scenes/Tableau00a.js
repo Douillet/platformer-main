d490603e10a31d09a974c6f9221d5032d52fc4b2
Douillet <douillet.a09092000@gmail.com>
2021-01-22 10:56:10 +0100

    Initial commit

---
+++ src/scenes/Tableau00a.js
@@ -0,0 +1,73 @@
+class Tableau00a extends Tableau{
+
+    preload() {
+        super.preload();
+        this.load.image('star', 'assets/star.png');
+        this.load.image('ground', 'assets/platform.png');
+    }
+    create() {
+        super.create();
+
+        
+
+        //des étoiles
+        this.stars=this.physics.add.group();
+        this.stars.create(100,0,"star");
+        this.stars.create(200,0,"star");
+        this.stars.create(300,0,"star");
+        this.stars.create(400,0,"star");
+        this.stars.create(500,0,"star");
+        this.stars.create(600,0,"star");
+        this.stars.create(700,0,"star");
+        this.stars.children.iterate(function (child) {
+            child.setCollideWorldBounds(true);
+            child.setBounce(0);
+        });
+        //quand le joueur touche une étoile on appelle la fonction ramasserEtoile
+        //this.physics.add.overlap(this.player, this.stars, this.ramasserEtoile, null, this);
+        
+        
+        this.platforms = this.physics.add.group();
+        this.platforms.create(200, 150, 'ground')
+        this.platforms.create(400, 250, 'ground')
+        this.platforms.create(600, 350, 'ground')
+
+        this.platforms.children.iterate(function (child) {
+            child.setDisplaySize(140,8);
+            child.setImmovable(true);
+            child.body.allowGravity=false;
+            child.setVelocityY(100);
+            child.setBounceY(1);
+            child.setCollideWorldBounds(true);
+            child.setFriction(0); //les éléments ne glissent pas dessus cette plateforme
+        });
+
+        let rouge=this.physics.add.sprite(10,200,"ground");
+        rouge.setDisplaySize(50,50)//taille de l'objet
+        rouge.setTintFill(0xFF0000);//applique une couleur rouge
+        rouge.setOrigin(0,0);//pour positionner plus facilement
+        rouge.body.allowGravity= 0; //la gravité n'a pas d'effet ici
+        rouge.setImmovable(true); //ne bouge pas quand on rentre dedans
+
+        //rouge.create(0, 0, 'ground');
+
+        this.physics.add.collider(this.player, rouge);//le joueur rebondit dessus
+        this.physics.add.collider(this.stars, rouge);//l'étoile1 rebondit dessus
+
+         //le joueur rebondit sur les plateformes
+         this.physics.add.collider(this.player, this.platforms);
+         //les étoiles rebondissent sur les plateformes
+         this.physics.add.collider(this.platforms, this.stars);
+         //test physique entre plateformes
+         this.physics.add.collider(this.stars);
+         //test rebond étoiles sur le joueur
+         this.physics.add.collider(this.stars, this.player);
+
+         this.physics.add.collider(this.platforms);         
+ 
+    }
+    update() {
+        super.update();
+    }
+
+}
