06dfb2539059b4b25d29df9af2d2d11321a761c0
Douillet <douillet.a09092000@gmail.com>
2021-02-01 14:43:25 +0100

    Create Geant.js

---
+++ src/objects/Geant.js
@@ -0,0 +1,15 @@
+class Geant extends ObjetEnnemi{
+
+    constructor(scene, x, y) {
+        super(scene, x, y, "Géant");
+
+        
+        this.setOrigin(0,0);
+        this.setDisplaySize(120,200);
+        this.setCollideWorldBounds(true);
+        this.setBounceY(0.2);
+        this.setBounceX(1);
+        this.setVelocityX(20);
+        this.physics.add.overlap(this.player, this, this.hitSpike, null, this);
+    }
+}
