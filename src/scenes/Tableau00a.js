5dad688b07e2e43e7bf23fb32718971c62369ec5
Douillet <douillet.a09092000@gmail.com>
2021-03-05 08:03:41 +0100

    Halo lumineux et fct réparées

--- src/scenes/Tableau00a.js
+++ src/scenes/Tableau00a.js
@@ -11,12 +11,13 @@ class Tableau00a extends Tableau{
         this.load.image('troisiemeplan', 'assets/3e plan darbres.png');
         this.load.image('herbe', 'assets/herbe1.png');
         this.load.image('Gobelin_basique', 'assets/Gobelin_basique.png');
-        this.load.image('Géant', 'assets/Géant.png');
+        this.load.image('halo', 'assets/Halo_lumineuxv2.png');
+        this.load.image('luciole', 'assets/luciolev2.png');
     }
     create() {
         super.create();
         
-        let largeurDuTableau=4000;
+        let largeurDuTableau=2000;
         let hauteurDuTableau=448; //la hauteur est identique au cadre du jeu
         this.cameras.main.setBounds(0, 0, largeurDuTableau, hauteurDuTableau);
         this.physics.world.setBounds(0, 0, largeurDuTableau,  hauteurDuTableau);
@@ -40,8 +41,8 @@ class Tableau00a extends Tableau{
             child.setBounce(0);
         });
         //quand le joueur touche une étoile on appelle la fonction ramasserEtoile
-        //this.physics.add.overlap(this.player, this.stars, this.ramasserEtoile, null, this);
         
+        this.physics.add.overlap(this.player, this.stars, this.ramasserEtoile, null, this);
         
         /*this.platforms = this.physics.add.group();
         this.platforms.create(200, 150, 'ground')
@@ -58,25 +59,18 @@ class Tableau00a extends Tableau{
             child.setFriction(1); //les éléments ne glissent pas dessus cette plateforme
         });*/
 
-       /*  let rouge=this.physics.add.sprite(10,200,"ground");
-        rouge.setDisplaySize(50,50)//taille de l'objet
-        rouge.setTintFill(0xFF0000);//applique une couleur rouge
-        rouge.setOrigin(0,0);//pour positionner plus facilement
-        rouge.body.allowGravity= 0; //la gravité n'a pas d'effet ici
-        rouge.setImmovable(true); //ne bouge pas quand on rentre dedans**/
 
-        //rouge.create(0, 0, 'ground');
-
-        this.gobelin1 = new Gobelin_basique(this, 500, 250);
+        this.gobelin1=new Gobelin_basique(this, 500, 250);
+        
 
         this.solherbe = this.physics.add.group();
-        /**this.solherbe.children.iterate(function (child) {
+        this.solherbe.children.iterate(function (child) {
             child.setImmovable(true);
             child.body.allowGravity=false;
             child.setBounceX(1);
             child.setCollideWorldBounds(true);
             child.setFriction(1); //les éléments ne glissent pas dessus cette plateforme
-        });**/
+        });
         for(let posX=0;posX<largeurDuTableau;posX+=64){
             let plate=this.solherbe.create(posX ,416,"herbe");
             plate.setImmovable(true);
@@ -86,24 +80,23 @@ class Tableau00a extends Tableau{
         this.physics.add.collider(this.solherbe, this.player);
         this.physics.add.collider(this.solherbe, this.stars);
 
-        this.physics.add.collider(this.solherbe, this.Gobelin_basique);
+        this.physics.add.collider(this.solherbe, this.gobelin1);
         
-        //this.physics.add.collider(this.platforms, this.solherbe);
-
-        //this.physics.add.collider(this.player, rouge);//le joueur rebondit dessus
-        //this.physics.add.collider(this.stars, rouge);//l'étoile1 rebondit dessus
+        
 
         //le joueur rebondit sur les plateformes
-        //this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.player, this.platforms);
         //les étoiles rebondissent sur les plateformes
         //this.physics.add.collider(this.platforms, this.stars);
         
         //test physique entre plateformes
-        this.physics.add.collider(this.stars);
+
         //test rebond étoiles sur le joueur
-        //this.physics.add.collider(this.stars, this.player);
+        this.physics.add.collider(this.stars, this.player);
         //this.physics.add.collider(this.platforms);    
         
+
+        //fond
         this.fond=this.add.tileSprite(
             0,
             0,
@@ -114,7 +107,8 @@ class Tableau00a extends Tableau{
 
         this.fond.setOrigin(0,0);
         this.fond.setScrollFactor(0);//fait en sorte que le ciel ne suive pas la caméra
-
+        
+        //1er rang d'arbre
         this.arbre=this.add.tileSprite(
             0,
             0,
@@ -124,7 +118,8 @@ class Tableau00a extends Tableau{
         );
         this.arbre.setScrollFactor(0);
         this.arbre.setOrigin(0,0);
-
+        
+        //2ème rang d'arbre
         this.arbre2=this.add.tileSprite(
             0,
             0,
@@ -134,7 +129,8 @@ class Tableau00a extends Tableau{
         );
         this.arbre2.setScrollFactor(0);
         this.arbre2.setOrigin(0,0);
-
+        
+        //3eme rang d'arbre
         this.arbre3=this.add.tileSprite(
             0,
             0,
@@ -145,13 +141,26 @@ class Tableau00a extends Tableau{
         this.arbre3.setScrollFactor(0);
         this.arbre3.setOrigin(0,0);
         
-        //fait passer les éléments devant le ciel
-        //this.platforms.setDepth(10);
-        //rouge.setDepth(10);
-        this.solherbe.setDepth(10);
+        //Halos lumineux
+        this.halo1=this.add.tileSprite(400, 0, 700, 448, 'halo');
+        this.halo1.setOrigin(0,0);
+        this.halo2=this.add.tileSprite(1200, 0, 700, 448, 'halo');
+        this.halo2.setOrigin(0,0);
+
+        //profondeur à 10 pour tous les ennemis
         this.gobelin1.setDepth(10);
+
+
+        //fait passer les éléments devant le ciel
+        this.solherbe.setDepth(10);
         this.stars.setDepth(10);
         this.player.setDepth(10);
+
+        //éléments visuels premier plan
+        this.halo1.setDepth(9);
+        this.halo2.setDepth(9)
+
+        //depht décor
         this.arbre.setDepth(9);
         this.arbre2.setDepth(8);
         this.arbre3.setDepth(7);
@@ -160,6 +169,10 @@ class Tableau00a extends Tableau{
     }
     update() {
         super.update();
+
+        //les éléments ont leur vitesse propre
+        
+
 
         //les arbres se déplacent moins vite pour accentuer l'effet
         this.arbre.tilePositionX=this.cameras.main.scrollX*0.5+100;
--- src/scenes/structure/Tableau.js
+++ src/scenes/structure/Tableau.js
@@ -62,13 +62,48 @@ class Tableau extends Phaser.Scene{
         if(totalActive===0){
             this.win();
         }
-        /*
-        // this.stars est un groupe (plus tard)
-        if (this.stars.countActive(true) === 0)
-        {
-           this.win();
+        
+    }
+
+    hitMonster(player, monster){
+        let me=this;
+        if(monster.isDead !== true){ //si notre monstre n'est pas déjà mort
+            if(
+                // si le player descend
+                player.body.velocity.y > 0
+                // et si le bas du player est plus haut que le monstre
+                && player.getBounds().bottom < monster.getBounds().top+30
+
+            ){
+                ui.gagne();
+                monster.isDead=true; //ok le monstre est mort
+                monster.disableBody(true,true);//plus de collisions
+                //this.saigne(monster,function(){
+                    //à la fin de la petite anim...ben il se passe rien :)
+                //})
+                //notre joueur rebondit sur le monstre
+                player.directionY=500;
+            }else{
+                //le joueur est mort
+                if(!me.player.isDead){
+                    me.player.isDead=true;
+                    me.player.visible=false;
+                    me.scene.restart();
+                    //ça saigne...
+                    /**me.saigne(me.player,function(){
+                        //à la fin de la petite anim, on relance le jeu
+                        me.blood.visible=false;
+                        me.player.anims.play('turn');
+                        me.player.isDead=false;
+                        me.scene.restart();
+                    })**/
+
+                }
+
+
+            }
         }
-         */
+
     }
 
     /**
