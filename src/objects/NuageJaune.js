a483bdf68a3bd352e201f3255f6071ac502bd4d7
Douillet <douillet.a09092000@gmail.com>
2021-05-26 16:55:59 +0200

    problème de blendmode

--- src/objects/NuageJaune.js
+++ src/objects/NuageJaune.js
@@ -8,7 +8,7 @@ class NuageJaune extends ObjetPhysique{
         //gestion de la taille
         this.setDisplaySize(200,138);
         //blendmode
-        this.blendMode='NORMAL';
+        this.blendMode='ADD';
 
         //on réduit un peu la zone de hit
         /*this.setBodySize(this.body.width-60,this.body.height-60);
